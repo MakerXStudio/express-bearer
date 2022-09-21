df4d3e3d95aa9d48d12e5f6c99e280deff4d962b
Sam Curry <sam.curry@makerx.com.au>
2022-09-21 13:14:32 +0800

    use JwtPayload for req.user type

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Express {
     interface Request {
-      user?: Record<string, unknown>
+      user?: JwtPayload
     }
   }
 }
